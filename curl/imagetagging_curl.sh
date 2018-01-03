d50af07869be07e377ea9b91e8901053baf0597f
vdust.leo <34883346+vdustleo@users.noreply.github.com>
2018-01-03 21:42:33 +0800

    Update imagetagging_curl.sh

--- curl/imagetagging_curl.sh
+++ curl/imagetagging_curl.sh
@@ -5,10 +5,13 @@
 #
 TOKEN=''
 
-curl -X POST https://ais.cn-north-1.myhwclouds.com/v1.0/ocr/action/ocr_form \
+curl -X POST https://ais.cn-north-1.myhwclouds.com/v1.0/image/tagging \
   --header 'Content-Type: application/json' \
-  --header 'X-Auth-Token: $TOKEN' -d '
-{
-    "url":""
-}
-'
+  --header "X-Auth-Token: $TOKEN" -d '
+ {
+      "image":"",
+      "url":"http://obs-hqq.obs.myhwclouds.com/tagging-normal",
+      "language": "zh",
+      "limit": 5,
+      "threshold": 30.0
+}'
