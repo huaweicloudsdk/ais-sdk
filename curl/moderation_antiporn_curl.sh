ccd315967bdbe7888eb63e0299e007e7f08f38b1
vdust.leo <34883346+vdustleo@users.noreply.github.com>
2018-01-05 21:03:28 +0800

    Update moderation_antiporn_curl.sh

--- curl/moderation_antiporn_curl.sh
+++ curl/moderation_antiporn_curl.sh
@@ -1,1 +1,14 @@
+#!/bin/sh
 
+#
+# Here, if we get the token use the gettoken_curl.sh
+#
+TOKEN=''
+
+curl -X POST https://ais.cn-north-1.myhwclouds.com/v1.0/moderation/image/anti-porn \
+  --header 'Content-Type: application/json' \
+  --header "X-Auth-Token: $TOKEN" -d '
+ {
+      "image":"",
+      "url":"http://obs-hqq.obs.myhwclouds.com/tagging-normal"
+}'
