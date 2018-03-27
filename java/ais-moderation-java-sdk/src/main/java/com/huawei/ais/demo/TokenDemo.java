a5e8a85bee21ff1f7798f172eb44c6f9da9b9bdb
t00441163 <tianxin14@huawei.com>
2018-03-27 20:16:19 +0800

    moderation sdk新增子服务：    1，清晰度检测    2，扭曲矫正

--- java/ais-moderation-java-sdk/src/main/java/com/huawei/ais/demo/TokenDemo.java
+++ java/ais-moderation-java-sdk/src/main/java/com/huawei/ais/demo/TokenDemo.java
@@ -98,27 +98,6 @@ public class TokenDemo {
 		return xst[0].getValue();
 
 	}
-
-	/**
-	 * 使用Base64编码后的文件方式，使用Token认证方式访问服务
-	 * @param token token认证串
-	 * @param formFile 文件路径
-	 * @throws IOException
-	 */
-	public static void requestModerationBase64(String token, String formFile) throws IOException {
-		String url = "https://ais.cn-north-1.myhuaweicloud.com/v1.0/moderation/anti-porn";
-		Header[] headers = new Header[] {new BasicHeader("X-Auth-Token", token) ,new BasicHeader("Content-Type", "application/json")};
-		String requestBody=toBase64Str(formFile);
-		StringEntity stringEntity = new StringEntity(requestBody, "utf-8");
-		try {
-			HttpResponse response = HttpClientUtils.post(url, headers, stringEntity);
-			System.out.println(response);
-			String content = IOUtils.toString(response.getEntity().getContent());
-			System.out.println(content);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 	
 	/**
 	 * 将二进制文件转为经Base64编码之后的请求访问对象（JSON串格式）
@@ -136,16 +115,114 @@ public class TokenDemo {
 		return json.toJSONString();
 	}
 
+	
+
+	/**
+	 * 内容检测，使用Base64编码后的文件方式，使用Token认证方式访问服务
+	 * @param token 
+	 * 			token认证串
+	 * @param formFile 
+	 * 			文件路径
+	 * @throws IOException
+	 */
+	public static void requestModerationBase64(String token, String formFile) throws IOException {
+		String url = "https://ais.cn-north-1.myhuaweicloud.com/v1.0/moderation/image/anti-porn";
+		Header[] headers = new Header[] {new BasicHeader("X-Auth-Token", token) ,new BasicHeader("Content-Type", "application/json")};
+		String requestBody=toBase64Str(formFile);
+		StringEntity stringEntity = new StringEntity(requestBody, "utf-8");
+		try {
+			HttpResponse response = HttpClientUtils.post(url, headers, stringEntity);
+			System.out.println(response);
+			String content = IOUtils.toString(response.getEntity().getContent());
+			System.out.println(content);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 清晰度检测，使用Base64编码后的文件方式，使用Token认证方式访问服务
+	 * @param token 
+	 * 			token认证串
+	 * @param formFile 
+	 * 			文件路径
+	 * @throws IOException
+	 */
+	public static void requestModerationClarityBase64(String token, String formFile) throws IOException {
+		String url = "https://ais.cn-north-1.myhuaweicloud.com/v1.0/moderation/image/clarity-detect";
+		Header[] headers = new Header[] {new BasicHeader("X-Auth-Token", token) ,new BasicHeader("Content-Type", ContentType.APPLICATION_JSON.toString())};
+		try {
+			byte[] fileData = FileUtils.readFileToByteArray(new File(formFile));
+			String fileBase64Str = Base64.encodeBase64String(fileData);
+			JSONObject json = new JSONObject();
+			json.put("image", fileBase64Str);
+			json.put("threshhold", 0.8);
+						
+			StringEntity stringEntity = new StringEntity(json.toJSONString(), "utf-8");
+			HttpResponse response = HttpClientUtils.post(url, headers, stringEntity);
+			System.out.println(response);
+			String content = IOUtils.toString(response.getEntity().getContent());
+			System.out.println(content);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 扭曲矫正，使用Base64编码后的文件方式，使用Token认证方式访问服务
+	 * @param token 
+	 * 			token认证串
+	 * @param formFile 
+	 * 			文件路径
+	 * @throws IOException
+	 */
+	public static void requestModerationDistortionCorrectBase64(String token, String formFile) throws IOException {
+		String url = "https://ais.cn-north-1.myhuaweicloud.com/v1.0/moderation/image/distortion-correct";
+		Header[] headers = new Header[] {new BasicHeader("X-Auth-Token", token) ,new BasicHeader("Content-Type", ContentType.APPLICATION_JSON.toString())};
+		try {
+			byte[] fileData = FileUtils.readFileToByteArray(new File(formFile));
+			String fileBase64Str = Base64.encodeBase64String(fileData);
+			JSONObject json = new JSONObject();
+			json.put("image", fileBase64Str);
+			json.put("correction", true);
+			StringEntity stringEntity = new StringEntity(json.toJSONString(), "utf-8");
+			
+			HttpResponse response = HttpClientUtils.post(url, headers, stringEntity);
+			
+			if(ResponseProcessUtils.isRespondedOK(response)) {
+				ResponseProcessUtils.processResponseWithImage(response, "data/moderation-distortion.corrected.jpg");
+			} else {
+				// 5.处理服务返回的字符流，输出识别结果。
+				ResponseProcessUtils.processResponseStatus(response);
+				String content = IOUtils.toString(response.getEntity().getContent());
+				System.out.println(content);
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
 	/**
 	 * 调用主入口函数
 	 */
 	public static void main(String[] args) throws URISyntaxException, UnsupportedOperationException, IOException {
-		String username = "zhangshan";    // 此处，请输入用户名
-		String password = "*******";	  // 此处，请输入对应用户名的密码
+		String username = "***";    // 此处，请输入用户名
+		String password = "***";	  // 此处，请输入对应用户名的密码
 		String projectName = "cn-north-1"; // 此处，请输入服务的区域信息，参考地址: http://developer.huaweicloud.com/dev/endpoint
 		String token = getToken(username, password, projectName);
 		System.out.println(token);
-		requestModerationBase64(token, "data/moderation-demo-1.jpg");
+		
+		//运行内容检测服务
+		//requestModerationBase64(token, "data/moderation-demo-1.jpg");
+		
+		//运行清晰度检测服务
+		//requestModerationClarityBase64(token, "data/moderation-demo-1.jpg");
+		
+		//运行扭曲矫正服务
+		//requestModerationDistortionCorrectBase64(token, "data/moderation-demo-1.jpg");
 
 	}
 
---
+++ java/ais-moderation-java-sdk/src/main/java/com/huawei/ais/demo/moderation/ModerationdDistortionCorrectDemo.java
@@ -0,0 +1,79 @@
+package com.huawei.ais.demo.moderation;
+
+import com.alibaba.fastjson.JSONObject;
+import com.huawei.ais.demo.ClientContextUtils;
+import com.huawei.ais.demo.ResponseProcessUtils;
+import com.huawei.ais.sdk.AisAccess;
+import com.huawei.ais.sdk.AisAccessWithProxy;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.FileUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.entity.StringEntity;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *  扭曲矫正服务的使用示例类
+ */
+public class ModerationdDistortionCorrectDemo {
+	//
+	// 扭曲矫正服务的使用示例函数
+	//
+	private static void moderationdDistortionCorrectDemo() throws IOException {
+		//
+		// 1. 在ClientContextUtils类中, 配置好访问扭曲矫正服务的基本信息,
+		// 然后，在此处生成对应的一个客户端连接对象
+		//
+		AisAccess service = new AisAccess(ClientContextUtils.getAuthInfo());
+
+		//
+		// 1.a 此处支持使用代理方式访问扭曲矫正服务，用于不能直接访问华为云官网服务的情况, 例如，内网网络。
+		// 如果使用此处方式，需要同时在ClientContextUtils中，配置相应的代理服务器的参数类(ProxyHostInfo)
+		//
+		//AisAccess service = new AisAccessWithProxy(ClientContextUtils.getAuthInfo(), ClientContextUtils.getProxyHost());
+
+		try {
+			//
+			// 2.构建访问扭曲矫正服务需要的参数
+			//
+			String uri = "/v1.0/moderation/image/distortion-correct";
+			byte[] fileData = FileUtils.readFileToByteArray(new File("data/moderation-demo-1.jpg"));
+			String fileBase64Str = Base64.encodeBase64String(fileData);
+
+			JSONObject json = new JSONObject();
+			json.put("image", fileBase64Str); //检测图片
+			json.put("correction", true); //判断是否清晰的阈值
+			StringEntity stringEntity = new StringEntity(json.toJSONString(), "utf-8");
+
+			// 3.传入扭曲矫正服务对应的uri参数, 传入扭曲矫正服务需要的参数，
+			// 该参数主要通过JSON对象的方式传入, 使用POST方法调用服务
+			HttpResponse response = service.post(uri, stringEntity);
+
+			// 4.验证服务调用返回的状态是否成功，如果为200, 为成功, 否则失败。
+			if(ResponseProcessUtils.isRespondedOK(response)) {
+				ResponseProcessUtils.processResponseWithImage(response, "data/moderation-distortion.corrected.jpg");
+			} else {
+				// 5.处理服务返回的字符流，输出识别结果。
+				ResponseProcessUtils.processResponseStatus(response);
+				ResponseProcessUtils.processResponse(response);
+			}
+						
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+
+			// 6.使用完毕，关闭服务的客户端连接			
+			service.close();
+		}
+	}
+
+	//
+	// 主入口函数
+	//
+	public static void main(String[] args) throws IOException {
+
+		// 测试入口函数
+		moderationdDistortionCorrectDemo();
+	}
+}
