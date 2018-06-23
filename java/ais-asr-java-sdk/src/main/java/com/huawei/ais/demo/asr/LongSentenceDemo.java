972e6772a677828cd666399cb732411d2999d8d4
AnyinSong <sglhsay@hotmail.com>
2018-06-23 17:52:12 +0800

    长语音AK/SK方式demo增加OBS方式提交音频示例。

--- java/ais-asr-java-sdk/src/main/java/com/huawei/ais/demo/asr/LongSentenceDemo.java
+++ java/ais-asr-java-sdk/src/main/java/com/huawei/ais/demo/asr/LongSentenceDemo.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 
 import com.huawei.ais.sdk.AisAccessWithProxy;
+import com.huawei.ais.demo.obs.SimpleObsClient;
+import com.huawei.ais.demo.obs.ObsFileHandle;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.FileUtils;
@@ -34,15 +36,15 @@ public class LongSentenceDemo {
 		// 设置三个超时参数限制连接超时，分别如下
 		int connectionTimeout = 5000; //连接目标url超时限制
 		int connectionRequestTimeout = 1000;//连接池获取可用连接超时限制
-		int socketTimeout = 5000;//获取服务器响应数据超时限制
+		int socketTimeout = 20000;//获取服务器响应数据超时限制
 		AisAccess service = new AisAccess(ClientContextUtils.getAuthInfo(), connectionTimeout,connectionRequestTimeout,socketTimeout);
-
+		SimpleObsClient simpleObsClient = new SimpleObsClient();
 		//
 		// 1.a 此处支持使用代理方式访问语音识别服务，用于不能直接访问华为云官网服务的情况, 例如，内网网络。
 		// 如果使用此处方式，需要同时在ClientContextUtils中，配置相应的代理服务器的参数类(ProxyHostInfo)
 		//
 		//AisAccess service = new AisAccessWithProxy(ClientContextUtils.getAuthInfo(), ClientContextUtils.getProxyHost(), connectionTimeout, connectionRequestTimeout, socketTimeout);
-
+		//SimpleObsClient simpleObsClient = new SimpleObsClient(ClientContextUtils.getProxyHost());
 		try {
 
 			//
@@ -53,7 +55,21 @@ public class LongSentenceDemo {
 			String fileBase64Str = Base64.encodeBase64String(fileData);
 
 			JSONObject json = new JSONObject();
-			json.put("data", fileBase64Str);
+
+			json.put("data", fileBase64Str); //如果音频超过10MB，请使用OBS方式，注释掉此行
+
+			//String bucketName = "asr-sdk-test";
+			//如果目标桶已存在，则不需要执行创建桶的动作
+			//simpleObsClient.createBucket(bucketName);
+
+			//将音频上传到OBS中，生成临时授权下载链接，提交给语音识别服务
+			//ObsFileHandle obsFileHandle = simpleObsClient.uploadFile(bucketName, "data/sentence.wav");
+
+			//如果音频已在OBS中，直接获取其句柄生成临时授权下载链接，提交给语音识别服务
+			//ObsFileHandle obsFileHandle = simpleObsClient.locateFile(bucketName, "sentence.wav");
+
+			//json.put("url", obsFileHandle.generateSharedDownloadUrl());
+
 
 			// 3.传入长语音识别服务对应的uri参数, 传入长语音识别服务需要的参数，
 			// 该参数主要通过JSON对象的方式传入, 使用POST方法调用服务
@@ -96,6 +112,9 @@ public class LongSentenceDemo {
 				{
 					// 7. 处理服务返回的字符流，输出识别结果。
 					words = getProcessResult(result);
+
+					//可选动作，音频识别结束，将音频从obs中删除
+					//obsFileHandle.delete();
 					break;
 				}
 				// status == 0 || status == 1
@@ -113,6 +132,7 @@ public class LongSentenceDemo {
 		} finally {
 			// 8.使用完毕，关闭服务的客户端连接
 			service.close();
+			simpleObsClient.close();
 		}
 	}
 
