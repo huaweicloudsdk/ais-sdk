2743da639c04f8fa065a1310de1dff6a18a31f46
vdust.leo <17091412@qq.com>
2018-07-04 15:30:43 +0800

    add support for download url for python moderation sdk

--- python/rest-client-samples/moderation/utils.py
+++ python/rest-client-samples/moderation/utils.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 import base64
+import urllib
 def encode_to_base64(filename):
     """
     encoding file to base64 encoded stream text
@@ -10,3 +11,6 @@ def encode_to_base64(filename):
     with open(filename, 'rb') as file:
         imgstr = base64.b64encode(file.read())
     return imgstr
+
+def download_url_base64(url):
+    return base64.b64encode(urllib.urlopen(url).read())
