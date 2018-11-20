933e7506504cb523904b928bb0648012c5dc78da
vdust.leo <17091412@qq.com>
2018-11-20 17:05:54 +0800

    curl版本代码代示例调整。

--- curl/defog_curl.sh
+++ curl/defog_curl.sh
@@ -8,7 +8,7 @@ curl -X POST https://ais.cn-north-1.myhuaweicloud.com/v1.0/vision/defog \
   --header 'Content-Type: application/json' \
   --header "X-Auth-Token: $TOKEN" -d '
  {
-      "image":"iVBORw0KGgoAAAANSUhEUgAAAbgAAAHCCAIAAAASEUtDAAAAB3RJTUUH3QEMEiEkuvy1dQAAIABJREFUeJw0u0ezZdmRpefuWxx1xbtPy3gvdETqTCCBLACFLuumNtKMM875Jz..", # change file to base64
+      "image":"iVBORw0KGgoAAAANSUhEUgAAAbgAAAHCCAIAAAASEUtDAAAAB3RJTUUH3QEMEiEkuvy1dQAAIABJREFUeJw0u0ezZdmRpefuWxx1xbtPy3gvdETqTCCBLACFLuumNtKMM875Jz..",
       "file":"",
       "gamma": "1.5",
       "natural_look": true
--- curl/tts_curl.sh
+++ curl/tts_curl.sh
@@ -12,7 +12,7 @@ curl -X POST https://ais.cn-north-1.myhuaweicloud.com/v1.0/voice/tts \
       "text":"This is a test sample",
       "voice_name":"xiaoyan",
       "volume":0,
-      "sample_rate":"16k"
+      "sample_rate":"16k",
       "speech_speed":0,
       "pitch_rate":0
 }'
